e73418cba22032db39b644546d7372d6a1cfd685
Jose <josealbertop11molino@gmail.com>
2026-02-22 20:03:29 -0600

    askdjwa

--- src/Helper.elm
+++ src/Helper.elm
@@ -4,22 +4,22 @@ import Html exposing (Html, div, h1, h2, h3, h4, h5, h6, text, a)
 import Html.Attributes exposing (href)
 
 --Ejercicio 1
-joinWords: String -> String -> String
+joinWords : String -> String -> String
 joinWords word1 word2 =
     word1 ++ word2
+
 --Ejercicio 2
-isUpperChars: List Char -> List Bool
-isUpperChars list=
+isUpperChars : List Char -> List Bool
+isUpperChars list =
     List.map Char.isUpper list
 
 --Ejercicio 3
-
-evalChars: List Char -> (Char->Bool) ->List Bool
-evalChars list funTrans=
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
     List.map funTrans list
 
 --Ejercicio 4
-headers: String -> Html msg
+headers : String -> Html msg
 headers param =
     div []
         [ h1 [] [ text param ]
@@ -32,4 +32,4 @@ headers param =
 
 hyperlink : String -> String -> Html msg
 hyperlink url label =
-    a [href url] [text label]
+    a [ href url ] [ text label ]
