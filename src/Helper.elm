ca4129c241d2f43ede497586d58c02af17822a47
Jose <josealbertop11molino@gmail.com>
2026-02-22 20:12:05 -0600

    isdwkasd

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,24 +1,40 @@
 module Helper exposing (..)
 
-import Html exposing (Html, div, h1, h2, h3, h4, h5, h6, text, a)
+import Html exposing (Html, a, div, h1, h2, h3, h4, h5, h6, text)
 import Html.Attributes exposing (href)
 
---Ejercicio 1
+
+
+-- Ejercicio 1
+
+
 joinWords : String -> String -> String
 joinWords word1 word2 =
     word1 ++ word2
 
---Ejercicio 2
+
+
+-- Ejercicio 2
+
+
 isUpperChars : List Char -> List Bool
 isUpperChars list =
     List.map Char.isUpper list
 
---Ejercicio 3
+
+
+-- Ejercicio 3
+
+
 evalChars : List Char -> (Char -> Bool) -> List Bool
 evalChars list funTrans =
     List.map funTrans list
 
---Ejercicio 4
+
+
+-- Ejercicio 4
+
+
 headers : String -> Html msg
 headers param =
     div []
@@ -29,6 +45,7 @@ headers param =
         , h5 [] [ text param ]
         , h6 [] [ text param ]
         ]
+
 
 hyperlink : String -> String -> Html msg
 hyperlink url label =
